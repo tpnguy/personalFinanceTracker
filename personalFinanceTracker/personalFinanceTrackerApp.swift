1ace9d19eec99287d35ab163045d24df9f94c79d
tpnguy <clubjuya2@gmail.com>
2026-03-25 11:39:22 -0700

    Initial Commit

---
+++ personalFinanceTracker/personalFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalFinanceTrackerApp.swift
+//  personalFinanceTracker
+//
+//  Created by Thienban Nguyen on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct personalFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
